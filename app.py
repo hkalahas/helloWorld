bbcaba68f02e0786e5732e71291f58ff0b1b9eaf
hkalahas <hkalahas@terpmail.umd.edu>
2024-02-20 00:13:23 -0500

    Added a new route-about.

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World from Haarika Kalahasti! This is my first code change'
